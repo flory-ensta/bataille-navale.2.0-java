903af77fe13591eec53bf52bf847a94de155a496
Pierre-Élisée Flory <pierre-elisee.flory@ensta-paris.fr>
2020-02-04 13:40:22 +0100

    improvement of boards display

--- TP/bataille-navale/src/main/java/ensta/TestBoard.java
+++ TP/bataille-navale/src/main/java/ensta/TestBoard.java
@@ -3,8 +3,9 @@ package ensta;
 public class TestBoard {
     public static void main(String[] args) {
         Board myBoard = new Board("myBoard");
-        myBoard.print();
+        // myBoard.print();
         Board myOtherBoard = new Board("myOtherBoard", 15);
-        myOtherBoard.print();
+        // myOtherBoard.print();
+        myOtherBoard.printBoards();
     }
 }
