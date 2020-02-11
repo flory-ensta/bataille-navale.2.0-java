3182df70fab394dcb5c3aa6f01c005d772101a01
Pierre-Élisée Flory <pierre-elisee.flory@ensta-paris.fr>
2020-02-11 10:38:14 +0100

    Exercice 3

--- TP/bataille-navale/src/main/java/ensta/Board.java
+++ TP/bataille-navale/src/main/java/ensta/Board.java
@@ -1,5 +1,6 @@
 package ensta;
 
+import java.io.FileOutputStream;
 import java.io.PrintStream;
 
 public class Board {
@@ -18,7 +19,7 @@ public class Board {
     }
 
     public Board(String name) {
-        this(name,10);
+        this(name, 10);
     }
 
     public int getGridSize() {
@@ -32,6 +33,14 @@ public class Board {
         }
         System.out.println("");
     }
+
+    // private initGrids() {
+    // for (int i = 0; i < size; i++)
+    // for (int j = 0; j < size; j++) {
+    // hits_array[i][j] = false;
+    // boats_array[i][j] = '.';
+    // }
+    // }
 
     private void printBoats() {
         printFirstLine();
@@ -64,11 +73,120 @@ public class Board {
         }
     }
 
+    public void printBoards() {
+        System.out.print("   ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(charArray[i].toString() + " ");
+        }
+        System.out.print("   ");
+        System.out.print("   ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(charArray[i].toString() + " ");
+        }
+        System.out.println("");
+
+        for (int i = 1; i < size + 1; i++) {
+            if (i < 10)
+                System.out.print(i + "  ");
+            else
+                System.out.print(i + " ");
+            for (int j = 0; j < size; j++) {
+                if (boats_array[i - 1][j] == null)
+                    System.out.print(". ");
+                else
+                    System.out.print(boats_array[i - 1][j] + " ");
+            }
+            System.out.print("   ");
+            if (i < 10)
+                System.out.print(i + "  ");
+            else
+                System.out.print(i + " ");
+            for (int j = 0; j < size; j++) {
+                if (!hits_array[i - 1][j])
+                    System.out.print(". ");
+                else
+                    System.out.print("x ");
+            }
+            System.out.println("");
+        }
+
+    }
+
     public void print() {
         System.out.println("Navires :");
         printBoats();
         System.out.println("Frappes :");
         printHits();
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    void putShip(AbstractShip ship, int x, int y) throws OutOfBound, IncorrectPosition {
+        x--;
+        if (x < 0 || x >= size || y < 0 || y >= size)
+            throw new OutOfBound("Positions incorrectes");
+        switch (ship.getDirection()) {
+        case NORTH:
+            if (x - ship.getSize() + 1 < 0)
+                throw new OutOfBound("Positions incorrectes");
+            for (int i = 0; i < ship.getSize(); i++)
+                if (this.boats_array[x - i][y] != null)
+                    throw new IncorrectPosition("A ship is already at this position");
+            for (int i = 0; i < ship.getSize(); i++)
+                this.boats_array[x - i][y] = ship.getLabel();
+            break;
+        case SOUTH:
+            if (x + ship.getSize() - 1 >= size)
+                throw new OutOfBound("Positions incorrectes");
+            for (int i = 0; i < ship.getSize(); i++)
+                if (this.boats_array[x + i][y] != null)
+                    throw new IncorrectPosition("A ship is already at this position");
+            for (int i = 0; i < ship.getSize(); i++)
+                this.boats_array[x + i][y] = ship.getLabel();
+            break;
+        case WEST:
+            if (y + ship.getSize() - 1 >= size)
+                throw new OutOfBound("Positions incorrectes");
+            for (int i = 0; i < ship.getSize(); i++)
+                if (this.boats_array[x][y + i] != null)
+                    throw new IncorrectPosition("A ship is already at this position");
+            for (int i = 0; i < ship.getSize(); i++)
+                this.boats_array[x][y + i] = ship.getLabel();
+            break;
+        case EAST:
+            if (y - ship.getSize() + 1 < 0)
+                throw new OutOfBound("Positions incorrectes");
+            for (int i = 0; i < ship.getSize(); i++)
+                if (this.boats_array[x][y - 1] != null)
+                    throw new IncorrectPosition("A ship is already at this position");
+            for (int i = 0; i < ship.getSize(); i++)
+                this.boats_array[x][y - i] = ship.getLabel();
+            break;
+        }
+
+    }
+
+    boolean hasShip(int x, int y) throws OutOfBound {
+        x--;
+        if (x < 0 || x >= size || y < 0 || y >= size)
+            throw new OutOfBound("Positions incorrectes");
+        return (boats_array[x][y] != null);
+    }
+
+    void setHit(boolean hit, int x, int y) throws OutOfBound {
+        x--;
+        if (x < 0 || x >= size || y < 0 || y >= size)
+            throw new OutOfBound("Positions incorrectes");
+        hits_array[x][y] = hit;
+    }
+
+    boolean getHit(int x, int y) throws OutOfBound {
+        x--;
+        if (x < 0 || x >= size || y < 0 || y >= size)
+            throw new OutOfBound("Positions incorrectes");
+        return hits_array[x][y];
     }
 
     public static void main(String[] args) {
--- TP/bataille-navale/src/main/java/ensta/TestBoard.java
+++ TP/bataille-navale/src/main/java/ensta/TestBoard.java
@@ -2,10 +2,27 @@ package ensta;
 
 public class TestBoard {
     public static void main(String[] args) {
-        Board myBoard = new Board("myBoard");
-        // myBoard.print();
+        // Board myBoard = new Board("myBoard");
         Board myOtherBoard = new Board("myOtherBoard", 15);
-        // myOtherBoard.print();
+        AbstractShip myShip = new Battleship(Direction.SOUTH);
+        AbstractShip myShip2 = new Carrier(Direction.WEST);
+        AbstractShip myShip3 = new Submarine(Direction.EAST);
+        AbstractShip myShip4 = new Destroyer(Direction.NORTH);
+        try {
+            myOtherBoard.putShip(myShip, 12, 7);
+            myOtherBoard.putShip(myShip2, 12, 2);
+            myOtherBoard.putShip(myShip3, 1, 3);
+            myOtherBoard.putShip(myShip4, 2, 10);
+            System.out.println(myOtherBoard.hasShip(1, 0));
+            System.out.println(myOtherBoard.hasShip(1, 1));
+            // System.out.println(myOtherBoard.hasShip(1, 15));
+            for (int i = 0; i < 15; i++) {
+                myOtherBoard.setHit((myOtherBoard.hasShip(1, i)), 1, i);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        System.out.println(myShip.toString());
         myOtherBoard.printBoards();
     }
 }
